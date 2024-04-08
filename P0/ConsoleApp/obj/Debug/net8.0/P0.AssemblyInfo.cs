178cdcd10c97273d939800c1b62318c1d2ee2878
Kung Lo <vashdash25@gmail.com>
2024-04-07 22:56:18 -0400

    Deleted build files

--- P0/ConsoleApp/obj/Debug/net8.0/P0.AssemblyInfo.cs
+++ P0/ConsoleApp/obj/Debug/net8.0/P0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcaaf544779572234edee6a79f85b049d2131ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad2ed3bf725f6579f369dbf8918e60b6fce7864")]
 [assembly: System.Reflection.AssemblyProductAttribute("P0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
